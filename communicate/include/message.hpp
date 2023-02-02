afe16d000f512d67a3c4336d63eb9f5f815ac066
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-02-01 16:55:34 -0800

    Revert "make message compatible between systems"
    This reverts commit 8ae913b2e757ba9fc5919f350b6e84bc6f5c8987.

--- communicate/include/message.hpp
+++ communicate/include/message.hpp
@@ -4,10 +4,7 @@
 #include <string>
 #include <memory.h>
 
-// This is the maximum number of characters in a message.
-// note that it it should never exceed 256.
 #define MAX_MSG_SIZE 200
-
 class Message
 {
 public:
@@ -16,7 +13,7 @@ public:
     std::string get_msg();
 
 private:
-    uint8_t length;
+    size_t length;
     char buf[MAX_MSG_SIZE];
 };
 #endif
--- communicate/src/message.cpp
+++ communicate/src/message.cpp
@@ -1,15 +1,14 @@
 #include "message.hpp"
 
-Message::Message(std::string msg) : length(static_cast<uint8_t>(msg.size()))
+Message::Message(std::string msg) : length(msg.size())
 {
-    memset(buf, 0, MAX_MSG_SIZE * sizeof(char));
     memcpy(buf, msg.c_str(), length * sizeof(char));
 }
 
 std::string Message::get_msg()
 {
     std::string ret = "";
-    for (uint8_t i = 0; i < length; ++i)
+    for (size_t i = 0; i < length; ++i)
     {
         ret.push_back(buf[i]);
     }
