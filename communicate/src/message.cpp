8ae913b2e757ba9fc5919f350b6e84bc6f5c8987
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-02-01 16:51:07 -0800

    make message compatible between systems

--- communicate/src/message.cpp
+++ communicate/src/message.cpp
@@ -1,14 +1,15 @@
 #include "message.hpp"
 
-Message::Message(std::string msg) : length(msg.size())
+Message::Message(std::string msg) : length(static_cast<uint8_t>(msg.size()))
 {
+    memset(buf, 0, MAX_MSG_SIZE * sizeof(char));
     memcpy(buf, msg.c_str(), length * sizeof(char));
 }
 
 std::string Message::get_msg()
 {
     std::string ret = "";
-    for (size_t i = 0; i < length; ++i)
+    for (uint8_t i = 0; i < length; ++i)
     {
         ret.push_back(buf[i]);
     }
