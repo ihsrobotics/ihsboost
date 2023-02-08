ae1fc279dce81243aa7cf18fcbb2d980254038a5
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-02-08 09:08:18 -0800

    fix encoder_... movement

--- movement/src/roomba_movement.cpp
+++ movement/src/roomba_movement.cpp
@@ -2,6 +2,8 @@
 #include "accelerator.hpp"
 #include <kipr/wombat.h>
 #include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 void rotate(double leftWheelSpeed, double rightWheelSpeed, double angle)
 {
@@ -30,12 +32,10 @@ void encoder_drive_straight(int max_speed, double cm, int min_speed, double corr
 
     LinearAccelerator accelerator(0, max_speed * sign_val, accel_per_sec, updates_per_sec);
 
-    while ((mm > 0 &&
-            (lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) < mm / 2 &&
-            (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8) < mm / 2) ||
-           ((mm < 0 &&
-             (lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) > mm / 2 &&
-             (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8) > mm / 2)))
+    while ((mm > 0 && (lenc_delta * (M_PI * 72.0 / 508.8) < mm / 2 &&
+                       renc_delta * (M_PI * 72.0 / 508.8) < mm / 2)) ||
+           (mm < 0 && (lenc_delta * (M_PI * 72.0 / 508.8) > mm / 2 &&
+                       renc_delta * (M_PI * 72.0 / 508.8) > mm / 2)))
     {
         // if left wheel going faster, go slower
         if ((mm > 0 && lenc_delta > renc_delta) || (mm < 0 && lenc_delta < renc_delta))
@@ -54,22 +54,22 @@ void encoder_drive_straight(int max_speed, double cm, int min_speed, double corr
 
         // update encoders
         _create_get_raw_encoders(&lenc_f, &renc_f);
+        lenc_delta = lenc_f - lenc_i;
+        renc_delta = renc_f - renc_i;
 
         if (accelerator.done())
         {
-            cached_distance = ((lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) + (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8)) / 2.0;
+            cached_distance = (lenc_delta * (M_PI * 72.0 / 508.8) + renc_delta * (M_PI * 72.0 / 508.8)) / 2.0;
             break;
         }
     }
 
     // do any more driving until it is time to start decelerating
     while (cached_distance != 0 &&
-           ((mm > 0 &&
-             (lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) < mm - cached_distance &&
-             (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8) < mm - cached_distance) ||
-            ((mm < 0 &&
-              (lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) > mm - cached_distance &&
-              (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8) > mm - cached_distance))))
+           ((mm > 0 && (lenc_delta * (M_PI * 72.0 / 508.8) < mm - cached_distance &&
+                        renc_delta * (M_PI * 72.0 / 508.8) < mm - cached_distance)) ||
+            (mm < 0 && (lenc_delta * (M_PI * 72.0 / 508.8) > mm - cached_distance &&
+                        renc_delta * (M_PI * 72.0 / 508.8) > mm - cached_distance))))
     {
         // if left wheel going faster, go slower
         if ((mm > 0 && lenc_delta > renc_delta) || (mm < 0 && lenc_delta < renc_delta))
@@ -87,19 +87,20 @@ void encoder_drive_straight(int max_speed, double cm, int min_speed, double corr
 
         // update encoders
         _create_get_raw_encoders(&lenc_f, &renc_f);
+        lenc_delta = lenc_f - lenc_i;
+        renc_delta = renc_f - renc_i;
     }
 
     // start decelerating, go until both lenc and renc have reached the end
     LinearAccelerator decelerator(accelerator.speed(), min_speed * sign_val, accel_per_sec, updates_per_sec);
-    while ((mm > 0 &&
-                (lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) < mm ||
-            (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8) < mm) ||
-           ((mm < 0 &&
-                 (lenc_delta = lenc_f - lenc_i) * (M_PI * 72.0 / 508.8) > mm ||
-             (renc_delta = renc_f - renc_i) * (M_PI * 72.0 / 508.8) > mm)))
+    while ((mm > 0 && (lenc_delta * (M_PI * 72.0 / 508.8) < mm ||
+                       renc_delta * (M_PI * 72.0 / 508.8) < mm)) ||
+           ((mm < 0 && (lenc_delta * (M_PI * 72.0 / 508.8) > mm ||
+                        renc_delta * (M_PI * 72.0 / 508.8) > mm))))
     {
         // both still have places to go
-        if (lenc_delta < mm && renc_delta < mm)
+        if ((mm > 0 && lenc_delta * (M_PI * 72.0 / 508.8) < mm && renc_delta * (M_PI * 72.0 / 508.8) < mm) ||
+            (mm < 0 && lenc_delta * (M_PI * 72.0 / 508.8) > mm && renc_delta * (M_PI * 72.0 / 508.8) > mm))
         {
             // if left wheel going faster, go slower
             if ((mm > 0 && lenc_delta > renc_delta) || (mm < 0 && lenc_delta < renc_delta))
@@ -113,14 +114,14 @@ void encoder_drive_straight(int max_speed, double cm, int min_speed, double corr
             }
         }
         // only lenc has places to go
-        else if (lenc_delta < mm)
+        else if ((mm > 0 && lenc_delta * (M_PI * 72.0 / 508.8) < mm) || (mm < 0 && lenc_delta * (M_PI * 72.0 / 508.8) > mm))
         {
-            create_drive_direct(min_speed * sign_val, 0);
+            create_drive_direct(min_speed * sign_val, sign_val);
         }
         // only renc has places to go
         else
         {
-            create_drive_direct(0, min_speed * sign_val);
+            create_drive_direct(sign_val, min_speed * sign_val);
         }
 
         // sleep
@@ -129,6 +130,8 @@ void encoder_drive_straight(int max_speed, double cm, int min_speed, double corr
 
         // update encoders
         _create_get_raw_encoders(&lenc_f, &renc_f);
+        lenc_delta = lenc_f - lenc_i;
+        renc_delta = renc_f - renc_i;
     }
     create_drive_direct(0, 0);
 }
@@ -136,7 +139,7 @@ void encoder_drive_straight(int max_speed, double cm, int min_speed, double corr
 void encoder_turn_degrees(int max_speed, int degrees, int min_speed, double accel_per_sec, int updates_per_sec)
 {
     // this function uses the following formula:
-    // angle in radians = (left wheel distance – right wheel distance) / wheel base distance.
+    // angle in radians = (left wheel distance (mm) – right wheel distance (mm)) / wheel base distance (mm).
 
     // initialize misc
     double cached_angle_degrees = 0;
@@ -152,8 +155,8 @@ void encoder_turn_degrees(int max_speed, int degrees, int min_speed, double acce
 
     LinearAccelerator accelerator(0, max_speed, accel_per_sec, updates_per_sec);
 
-    while ((degrees > 0 && (angle_degrees = ((lenc_delta = lenc_f - lenc_i) - (renc_delta = renc_f - renc_i)) / DIST_BETWEEN_WHEEL * rad2deg) < degrees / 2) ||
-           (degrees < 0 && (angle_degrees = ((lenc_delta = lenc_f - lenc_i) - (renc_delta = renc_f - renc_i)) / DIST_BETWEEN_WHEEL * rad2deg) > degrees / 2))
+    while ((degrees > 0 && angle_degrees < degrees / 2) ||
+           (degrees < 0 && angle_degrees > degrees / 2))
     {
         create_drive_direct(static_cast<int>(accelerator.speed() * left_sign_val), static_cast<int>(accelerator.speed() * right_sign_val));
 
@@ -163,18 +166,22 @@ void encoder_turn_degrees(int max_speed, int degrees, int min_speed, double acce
 
         // update encoders
         _create_get_raw_encoders(&lenc_f, &renc_f);
+        lenc_delta = lenc_f - lenc_i;
+        renc_delta = renc_f - renc_i;
+        angle_degrees = (lenc_delta * (M_PI * 72.0 / 508.8) - renc_delta * (M_PI * 72.0 / 508.8)) / (DIST_BETWEEN_WHEEL * 10) * rad2deg;
+        cout << "lenc delta is " << lenc_delta << " and renc delta is " << renc_delta << " and angle_degrees " << angle_degrees << endl;
 
         if (accelerator.done())
         {
-            cached_angle_degrees = (angle_degrees = ((renc_delta = renc_f - renc_i) - (lenc_delta = lenc_f - lenc_i)) / DIST_BETWEEN_WHEEL * rad2deg);
+            cached_angle_degrees = angle_degrees;
             break;
         }
     }
 
     // do any more driving until it is time to start decelerating
     while (cached_angle_degrees != 0 &&
-           ((degrees > 0 && (angle_degrees = ((lenc_delta = lenc_f - lenc_i) - (renc_delta = renc_f - renc_i)) / DIST_BETWEEN_WHEEL * rad2deg) < degrees - cached_angle_degrees) ||
-            (degrees < 0 && (angle_degrees = ((lenc_delta = lenc_f - lenc_i) - (renc_delta = renc_f - renc_i)) / DIST_BETWEEN_WHEEL * rad2deg) > degrees - cached_angle_degrees)))
+           ((degrees > 0 && angle_degrees < degrees - cached_angle_degrees) ||
+            (degrees < 0 && angle_degrees > degrees - cached_angle_degrees)))
     {
         create_drive_direct(static_cast<int>(accelerator.speed() * left_sign_val), static_cast<int>(accelerator.speed() * right_sign_val));
 
@@ -183,12 +190,16 @@ void encoder_turn_degrees(int max_speed, int degrees, int min_speed, double acce
 
         // update encoders
         _create_get_raw_encoders(&lenc_f, &renc_f);
+        lenc_delta = lenc_f - lenc_i;
+        renc_delta = renc_f - renc_i;
+        angle_degrees = (lenc_delta * (M_PI * 72.0 / 508.8) - renc_delta * (M_PI * 72.0 / 508.8)) / (DIST_BETWEEN_WHEEL * 10) * rad2deg;
+        cout << "lenc delta is " << lenc_delta << " and renc delta is " << renc_delta << " and angle_degrees " << angle_degrees << endl;
     }
 
     // start decelerating, go until both lenc and renc have reached the end
     LinearAccelerator decelerator(accelerator.speed(), min_speed, accel_per_sec, updates_per_sec);
-    while ((degrees > 0 && (angle_degrees = ((lenc_delta = lenc_f - lenc_i) - (renc_delta = renc_f - renc_i)) / DIST_BETWEEN_WHEEL * rad2deg) < degrees) ||
-           (degrees < 0 && (angle_degrees = ((lenc_delta = lenc_f - lenc_i) - (renc_delta = renc_f - renc_i)) / DIST_BETWEEN_WHEEL * rad2deg) > degrees))
+    while ((degrees > 0 && angle_degrees < degrees) ||
+           (degrees < 0 && angle_degrees > degrees))
 
     {
         // both still have places to go
@@ -200,6 +211,10 @@ void encoder_turn_degrees(int max_speed, int degrees, int min_speed, double acce
 
         // update encoders
         _create_get_raw_encoders(&lenc_f, &renc_f);
+        lenc_delta = lenc_f - lenc_i;
+        renc_delta = renc_f - renc_i;
+        angle_degrees = (lenc_delta * (M_PI * 72.0 / 508.8) - renc_delta * (M_PI * 72.0 / 508.8)) / (DIST_BETWEEN_WHEEL * 10) * rad2deg;
+        cout << "lenc delta is " << lenc_delta << " and renc delta is " << renc_delta << " and angle_degrees " << angle_degrees << endl;
     }
     create_drive_direct(0, 0);
 }
