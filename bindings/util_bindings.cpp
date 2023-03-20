ba54c151a63419038c99cfdc4faabc5f90c6c59b
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-03-20 16:06:04 -0700

    binding get version

--- bindings/util_bindings.cpp
+++ bindings/util_bindings.cpp
@@ -11,6 +11,11 @@ namespace util_export
         o << "(" << s.left << ", " << s.right << ")";
         return o.str();
     }
+
+    std::string get_version()
+    {
+        return IHSBOOST_VERSION;
+    }
 }; // namespace util_export
 
 void export_util()
@@ -39,4 +44,5 @@ void export_util()
         .value("RightSide", LineSide::RightSide);
     def("set_extra_config", set_extra_config);
     def("set_default_config", set_default_config);
+    def("get_version", get_version);
 }
