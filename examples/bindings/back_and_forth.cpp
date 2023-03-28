845b5358db2d8a95862fd6328bc4dc10b32a1c06
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-03-27 18:09:43 -0700

    add missing defaults, improve example

--- examples/bindings/back_and_forth.cpp
+++ examples/bindings/back_and_forth.cpp
@@ -1,3 +1,17 @@
+/**
+ * @file back_and_forth.cpp
+ * @author Eliot Hall
+ * @brief example communication between python and
+ * c++ where the c++ program calls the python program.
+ * This can be run by merely compiling this and running
+ * the created executable
+ * @version 0.1
+ * @date 2023-03-27
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include <iostream>
 #include <ihsboost/all.hpp>
 #include <thread>
@@ -10,6 +24,18 @@ int main()
 {
     // we can use the same queue for back and forth communication
     PosixQCommunicator communicator("/my_queue");
+
+    // launch python program
+    system("python3 ./back_and_forth.py &");
+
+    // wait for python to let us know it is ready
+    cout << "waiting for python to be ready" << endl;
+    int status = communicator.receive_msg().get_val<int>();
+    if (status != 1)
+    {
+        return status;
+    }
+    cout << "received a 1 from python letting us know it's ready!!" << endl;
 
     cout << "seinding 33 to python!!" << endl;
     communicator.send_msg(communicator.create_msg<int>(33));
