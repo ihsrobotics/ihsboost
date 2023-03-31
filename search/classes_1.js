04e008a4e8469ae9616442ef17574201c680bc49
chrehall68 <chrehall68@users.noreply.github.com>
2023-03-31 22:56:09 +0000

    deploy: 55fa8bccd6e7ec46552bb3889af94a98b383dc57

--- search/classes_1.js
+++ search/classes_1.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['backgroundtask_236',['BackgroundTask',['../classBackgroundTask.html',1,'']]],
   ['badbufcastexception_237',['BadBufCastException',['../classBadBufCastException.html',1,'']]],
-  ['bluetoothclient_238',['BluetoothClient',['../classBluetoothClient.html',1,'']]],
-  ['bluetoothserver_239',['BluetoothServer',['../classBluetoothServer.html',1,'']]]
+  ['bluetoothclient_238',['BluetoothClient',['../classBluetoothClient.html',1,'BluetoothClient'],['../classihs__bindings_1_1BluetoothClient.html',1,'ihs_bindings.BluetoothClient']]],
+  ['bluetoothserver_239',['BluetoothServer',['../classBluetoothServer.html',1,'BluetoothServer'],['../classihs__bindings_1_1BluetoothServer.html',1,'ihs_bindings.BluetoothServer']]]
 ];
